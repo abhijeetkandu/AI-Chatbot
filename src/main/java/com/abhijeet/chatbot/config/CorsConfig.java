f18ce6db4e51f2ad020b8b7d9dbcfbed0a4f379d
Abhijeet <kanduabhi9408@gmail.com>
2026-03-27 14:16:15 +0530

    Changed the model.

--- src/main/java/com/abhijeet/chatbot/config/CorsConfig.java
+++ src/main/java/com/abhijeet/chatbot/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**")
+        registry.addMapping("/ai/**")
                 .allowedOrigins("*")
                 .allowedMethods("POST","GET","OPTIONS")
                 .allowedHeaders("*");
